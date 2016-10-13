b3cc05b85376a180161ee08ab3edbae4d1dfd7fb
xyz <yangzongze@gmail.com>
2016-10-13 14:03:47 +0800

    1) Add paredit-mode for racket file. 2) Add mouse-3 for copy or yank

--- lisp/init-windows-local.el
+++ lisp/init-windows-local.el
@@ -5,6 +5,15 @@
 (setq x-select-enable-clipboard t)
 ;; (set-clipboard-coding-system 'utf-16le-dos)
 
+;; mouse-3 copy or yank content
+(defun x-copy-yank ()
+  (interactive)
+  (if (use-region-p)
+      (kill-ring-save (mark) (point))
+    (yank)))
+
+(global-set-key (kbd "<mouse-3>") 'x-copy-yank)
+
 ;; for tex
 ;; (setq TeX-view-program-list
 ;;       `(("SumatraPDF"
